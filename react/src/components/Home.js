d09c8b2e2d49e5b958bb89b5ec321ceb818486b7
Keith Hubert <keith.hubert@gmail.com>
2017-06-29 18:50:16 -0400

    removed video

--- react/src/components/Home.js
+++ react/src/components/Home.js
@@ -10,13 +10,8 @@ const Home = (props) => {
             <div className="center">
             <a href="/users/sign_up">Sign Up</a>
           </div>
-              <video id="background-video" loop autoPlay>
-              <source src="https://s3.us-east-2.amazonaws.com/gathererbackground/animation.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-            </video>
         </div>
       </div>
-
 
       <div className="summary">
         <p>Gathering is the first of it’s kind pairer for
